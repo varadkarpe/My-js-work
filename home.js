231be1825ddd63e1272074567a07fa3068e5e76e
Varad Karpe <varad.karpe@gmail.com>
2020-07-08 22:31:32 +0530

    Update on 8th July

---
+++ home.js
@@ -0,0 +1,14 @@
+// console.log("Hello, World")
+// console.log("Hello, World")
+// console.log("Hello, World")
+// alert('Hi')
+
+//Variables
+var a = "Varad";
+console.log(a);
+var num =45;
+console.log(num)
+
+var entry = prompt('Enter a random value');
+entry = entry * entry;
+document.getElementById('paragraph').innerHTML = "Square of the given value is "+ entry;
